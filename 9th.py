ece46d225ea89fc0b07cd788c8c97f3e031091ad
Patric <suntriber@hotmail.com>
2021-12-10 15:47:21 +0100

    day 9 finally ok

--- 9th.py
+++ 9th.py
@@ -1,52 +1,5 @@
 DATA = [l for l in open('9th.txt', 'r').read().splitlines()]
 
-
-def get_data():
-    return [l for l in open('9th.txt', 'r').read().splitlines()]
-
-
-def part1():
-    data = get_data()
-    low_points = []
-
-    # check horizontal
-    # - if j != 0 check left ([i][j-1])
-    # - if j != len(data)-1 check right ([i][j+1])
-
-    # check vertical
-    # - if i != 0 check above ([i-1][j])
-    # - if i != len(data)-1 check below ([i+1][j])
-
-    if int(data[0][0]) < int(data[0][1]) and int(data[0][0]) < int(data[1][0]): # top left
-        low_points.append(int(int(data[0][0]))+1)
-    if int(data[0][99]) < int(data[0][98]) and int(data[0][99]) < int(data[1][99]): # top right
-        low_points.append(int(int(data[0][99]))+1)
-    if int(data[99][0]) < int(data[99][1]) and int(data[99][0]) < int(data[98][0]): # bottom left
-        low_points.append(int(int(data[0][0]))+1)
-    if int(data[99][99]) < int(data[99][98]) and int(data[99][99]) < int(data[98][99]): # bottom right
-        low_points.append(int(int(data[0][0]))+1)
-    
-    for i in range(1, 99): 
-        if int(data[i][0]) < int(data[i][1]) and int(data[i][0]) < int(data[i+1][0]) and int(data[i][0]) < int(data[i-1][0]): 
-            low_points.append(int(data[i][0])+1) # check leftmost column
-        
-        if int(data[i][99]) < int(data[i][98]) and int(data[i][99]) < int(data[i+1][99]) and int(data[i][99]) < int(data[i-1][99]):
-            low_points.append(int(data[i][99])+1) # check rightmost column
-        
-        if int(data[0][i]) < int(data[1][i]) and int(data[0][i]) < int(data[0][i+1]) and int(data[0][i]) < int(data[0][i-1]):
-            low_points.append(int(data[0][i])+1) # check top row
-        
-        if int(data[99][i]) < int(data[98][i]) and int(data[99][i]) < int(data[99][i+1]) and int(data[99][i]) < int(data[99][i-1]):
-            low_points.append(int(data[99][i])+1) # check bottom row
-
-    for i in range(1, 99):
-        for j in range(1, 99):
-            if int(data[i][j]) < int(data[i][j+1]) and int(data[i][j]) < int(data[i][j-1]) and int(data[i][j]) < int(data[i+1][j]) and int(data[i][j]) < int(data[i-1][j]):
-                low_points.append(int(data[i][j])+1)
-    
-    return(sum(low_points))
-
-
 def part2():
     new = []
     for line in DATA:
@@ -73,6 +26,22 @@ def find_cluster(matrix, i, j):
         return []
     matrix[i][j] = 0
     return [matrix[i][j]] + find_cluster(matrix, i-1, j) + find_cluster(matrix, i+1, j) + find_cluster(matrix, i, j-1) + find_cluster(matrix, i, j+1)
+
+
+def get_mod():
+    l = [l for l in open('9th.txt', 'r').read().splitlines()]
+    return ['9'*(len(l)+2)] + ['9'+s+'9' for s in l] + ['9'*(len(l)+2)]
+
+
+def part1():
+    data = get_mod()
+    low_points = []
+    for i in range(1, len(data)-1):
+        for j in range(1, len(data[0])-1):
+            n = int(data[i][j])
+            if n < int(data[i][j+1]) and n < int(data[i][j-1]) and n < int(data[i+1][j]) and n < int(data[i-1][j]):
+                low_points.append(int(data[i][j])+1)
+    return sum(low_points)
 
 
 if __name__ == "__main__":
